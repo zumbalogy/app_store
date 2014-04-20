317dd8afdf85d4dc591d963fd3fb3e0ea30fff4d
zumbalogy <intrapostal@gmail.com>
2014-04-20 14:22:19 -0400

    better soc and naming in js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,50 +1,72 @@
-var apple;
-var android;
+var apple_array;
+var android_array;
+
+var apple_table_head = '<thead><th>Stars</th><th>Title</th><th>Creator</th><th>Rating Count</th><th>Price</th></thead>'
+var android_table_head = '<thead><th>Stars</th><th>Title</th><th>Creator</th><th>Rating Count</th><th>Price</th><th>Downloads</th></thead>'
 
 function init_apple(data){
-    $('#results').html('<thead><th>Stars</th><th>Title</th><th>Creator</th><th>Rating Count</th><th>Price</th></thead> ')
-    var results = data.results
-    console.log(results)
-    for (var i = 0; i < results.length; i++){
+    apple_array = data.results
+    render_apple_array()
+}
+
+function render_apple_array(){
+    $('#results').html(apple_table_head)
+    for (var i = 0; i < apple_array.length; i++){
         tr = $('<tr>')
-        tr.append("<td>" + results[i].averageUserRating + '</td>')
-        tr.append("<td>" + results[i].trackName + '</td>')
-        tr.append("<td>" + results[i].artistName + '</td>')
-        tr.append("<td>" + results[i].userRatingCount + '</td>')
-        tr.append("<td>" + results[i].price + '</td>')
+        tr.append("<td>" + apple_array[i].averageUserRating + '</td>')
+        tr.append("<td>" + apple_array[i].trackName + '</td>')
+        tr.append("<td>" + apple_array[i].artistName + '</td>')
+        tr.append("<td>" + apple_array[i].userRatingCount + '</td>')
+        tr.append("<td>" + apple_array[i].price + '</td>')
         $('#results').append(tr)
     }
 }
 
-function init_andriod(data){
-    $('#android-results').html('<thead><th>Stars</th><th>Title</th><th>Creator</th><th>Rating Count</th><th>Price</th><th>Downloads</th></thead> ')
-    var results = data.results
-    console.log(results)
-    for (var i = 0; i < results.length; i++){
-        var stars = Math.round(results[i].rating * 1000) / 1000
+function init_android(data){
+    android_array = data.results
+    render_android_array()
+}
+
+function render_android_array(){
+    $('#android-results').html(android_table_head)
+    for (var i = 0; i < android_array.length; i++){
+        var stars = Math.round(android_array[i].rating * 1000) / 1000
         tr = $('<tr>')
         tr.append("<td>" + stars + '</td>')
-        tr.append("<td>" + results[i].title + '</td>')
-        tr.append("<td>" + results[i].developer + '</td>')
-        tr.append("<td>" + results[i].number_ratings + '</td>')
-        tr.append("<td>" + results[i].price_numeric + '</td>')
-        tr.append("<td>" + results[i].downloads + '</td>')
+        tr.append("<td>" + android_array[i].title + '</td>')
+        tr.append("<td>" + android_array[i].developer + '</td>')
+        tr.append("<td>" + android_array[i].number_ratings + '</td>')
+        tr.append("<td>" + android_array[i].price_numeric + '</td>')
+        tr.append("<td>" + android_array[i].downloads + '</td>')
         $('#android-results').append(tr)
     }
 }
 
-$('#form').on('submit', function(event){
-    event.preventDefault();
+function apple_ajax(input){
     $.ajax({
-        url: "https://itunes.apple.com/search?term=" + $('#input').val() + "&country=us&entity=software",
-        type: 'get',
-        dataType: 'jsonp',
-        success: init_apple(data)
-    })
-    $.ajax({
-        url: "https://42matters.com/api/1/apps/search.json?q=" + $('#input').val() + "&limit=10 &access_token=df3b621f461a2f14b63f573c29575758c80d6d03",
+        url: "https://itunes.apple.com/search?term=" + input + "&country=us&entity=software",
         method: 'get',
         dataType: 'jsonp',
-        success: init_android(data)
+        success: function(data){
+            init_apple(data)
+        }
     })
+}
+
+function android_ajax(input){
+    $.ajax({
+        url: "https://42matters.com/api/1/apps/search.json?q=" + input + "&limit=10 &access_token=df3b621f461a2f14b63f573c29575758c80d6d03",
+        method: 'get',
+        dataType: 'jsonp',
+        success: function(data){
+            init_android(data)
+        }
+    })
+}
+
+$('#form').on('submit', function(event){
+    event.preventDefault()
+    var input = $('#input').val()
+    apple_ajax(input)
+    android_ajax(input)
 })
