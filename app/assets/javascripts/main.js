7b317b920acefe16b34f84b534696563f1995d60
zumbalogy <intrapostal@gmail.com>
2014-04-21 14:21:28 -0400

    now calc and show how many for each star rating for each version *of the reviews I am able to pull down

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -136,6 +136,7 @@ function render_apple_ul(){
 
             var version = app.reviews[x]['im:version'].label
             var content = app.reviews[x].content.label
+            var rating = parseInt(app.reviews[x]['im:rating'].label)
             if (word_count[version]){
                 word_count[version]['count'] ++
             } else {
@@ -144,7 +145,12 @@ function render_apple_ul(){
             this_count = word_count[version]
             word_count[version] = {
                 count: word_count[version]['count'],
-                stars: (this_count.stars || 0) + parseInt(app.reviews[x]['im:rating'].label),
+                stars: (this_count.stars || 0) + rating,
+                one_star: (this_count.one_star || 0) + (rating === 1 ? 1 : 0),
+                two_star: (this_count.two_star || 0) + (rating === 2 ? 1 : 0),
+                three_star: (this_count.three_star || 0) + (rating === 3 ? 1 : 0),
+                four_star: (this_count.four_star || 0) + (rating === 4 ? 1 : 0),
+                five_star: (this_count.five_star || 0) + (rating === 5 ? 1 : 0),
                 error: (this_count.error || 0) + (content.search(/error/i) > -1 ? 1 : 0),
                 bug: (this_count.bug || 0) + (content.search(/bug/i) > -1 ? 1 : 0),
                 crash: (this_count.crash || 0) + (content.search(/crash/i) > -1 ? 1 : 0),
@@ -171,31 +177,36 @@ function render_apple_ul(){
         }
 
         var word_table = $('<table>')
-        word_table.append('<th>Version</th><th>Review Count</th><th>Avg Stars</th><th>Error</th><th>Bug</th><th>Crash</th><th>Broke</th><th>Break</th><th>Fix</th><th>Slow</th><th>Freeze</th><th>Froze</th><th>Uninstall</th><th>Terrible</th><th>Bad</th><th>Worst</th><th>Worse</th><th>Hate</th><th>Suck</th>')
+        word_table.append('<th>Version</th><th>Review Count</th><th>Avg Stars</th><th>1 star</th><th>2 star</th><th>3 star</th><th>4 star</th><th>5 star</th><th>Error</th><th>Bug</th><th>Crash</th><th>Broke</th><th>Break</th><th>Fix</th><th>Slow</th><th>Freeze</th><th>Froze</th><th>Uninstall</th><th>Terrible</th><th>Bad</th><th>Worst</th><th>Worse</th><th>Hate</th><th>Suck</th>')
 
         for (var key in word_count){
             var obj = word_count[key]
             var avg_star = Math.round((obj.stars / obj.count) * 1000) / 1000
             var tr = $('<tr>')
-            tr.append('<td>' + key           + '</td>')
-            tr.append('<td>' + obj.count     + '</td>')
-            tr.append('<td>' + avg_star      + '</td>')
-            tr.append('<td>' + obj.error     + '</td>')
-            tr.append('<td>' + obj.bug       + '</td>')
-            tr.append('<td>' + obj.crash     + '</td>')
-            tr.append('<td>' + obj.broke     + '</td>')
-            tr.append('<td>' + obj.break     + '</td>')
-            tr.append('<td>' + obj.fix       + '</td>')
-            tr.append('<td>' + obj.slow      + '</td>')
-            tr.append('<td>' + obj.freeze    + '</td>')
-            tr.append('<td>' + obj.froze     + '</td>')
-            tr.append('<td>' + obj.uninstall + '</td>')
-            tr.append('<td>' + obj.terrible  + '</td>')
-            tr.append('<td>' + obj.bad       + '</td>')
-            tr.append('<td>' + obj.worst     + '</td>')
-            tr.append('<td>' + obj.worse     + '</td>')
-            tr.append('<td>' + obj.hate      + '</td>')
-            tr.append('<td>' + obj.suck      + '</td>')
+            tr.append('<td>' + key            + '</td>')
+            tr.append('<td>' + obj.count      + '</td>')
+            tr.append('<td>' + avg_star       + '</td>')
+            tr.append('<td>' + obj.one_star   + '</td>')
+            tr.append('<td>' + obj.two_star   + '</td>')
+            tr.append('<td>' + obj.three_star + '</td>')
+            tr.append('<td>' + obj.four_star  + '</td>')
+            tr.append('<td>' + obj.five_star  + '</td>')
+            tr.append('<td>' + obj.error      + '</td>')
+            tr.append('<td>' + obj.bug        + '</td>')
+            tr.append('<td>' + obj.crash      + '</td>')
+            tr.append('<td>' + obj.broke      + '</td>')
+            tr.append('<td>' + obj.break      + '</td>')
+            tr.append('<td>' + obj.fix        + '</td>')
+            tr.append('<td>' + obj.slow       + '</td>')
+            tr.append('<td>' + obj.freeze     + '</td>')
+            tr.append('<td>' + obj.froze      + '</td>')
+            tr.append('<td>' + obj.uninstall  + '</td>')
+            tr.append('<td>' + obj.terrible   + '</td>')
+            tr.append('<td>' + obj.bad        + '</td>')
+            tr.append('<td>' + obj.worst      + '</td>')
+            tr.append('<td>' + obj.worse      + '</td>')
+            tr.append('<td>' + obj.hate       + '</td>')
+            tr.append('<td>' + obj.suck       + '</td>')
             word_table.append(tr)
         }
 
