c3b5b881eb3a24bee30a77c0da8ec9d15f3bb395
zumbalogy <intrapostal@gmail.com>
2014-04-23 18:07:45 -0400

    added apple and andriod models and apple fields done for mongo

---
+++ app/models/androidapp.rb
@@ -0,0 +1,12 @@
+class AndroidApp
+    include Mongoid::Document
+    field :trackId, type: Integer
+    field :averageUserRating, type: Float
+    field :averageUserRatingForCurrentVersion, type: Float
+    field :trackName, type: String
+    field :artistName, type: String
+    field :userRatingCount, type: Integer
+    field :userRatingCountForCurrentVersion, type: Integer
+    field :price, type: Float
+    field :reviews, type: Array 
+end
